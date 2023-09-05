cff16b685ec0f867abaff20f7cf06e1e948c9867
benjamin GIRARD <benjamingirard25@gmail.com>
2023-09-05 17:21:49 +0200

    FileSystemRepository has new method

--- src/FileSystemMessageRepository.ts
+++ src/FileSystemMessageRepository.ts
@@ -4,14 +4,27 @@ import { Message } from "./Message";
 import { MessageRepository } from "./MessageRepository";
 
 export class FileSystemMessageRepository implements MessageRepository {
+  private messagePath = path.join(__dirname, "message.json");
+
   getAllOfUser(user: string): Promise<Message[]> {
     throw new Error("Method not implemented.");
   }
 
   async save(msg: Message): Promise<void> {
-    return fs.promises.writeFile(
-      path.join(__dirname, "message.json"),
-      JSON.stringify(msg)
-    );
+    const messages = await this.getMessages();
+    messages.push(msg);
+    return fs.promises.writeFile(this.messagePath, JSON.stringify(messages));
+  }
+
+  private async getMessages(): Promise<Message[]> {
+    const data = await fs.promises.readFile(this.messagePath);
+    const messages = JSON.parse(data.toString()) as Message[];
+
+    return messages.map((msg) => ({
+      id: msg.id,
+      text: msg.text,
+      author: msg.author,
+      publishedAt: new Date(msg.publishedAt),
+    }));
   }
 }
